a33be992f68fc45ab95b086a52ba058477501eef
a13x95 <kuscog@gmail.com>
2017-10-08 22:44:49 +0300

    Created some new patterns, nothing more

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
 import aiml
 
-
-
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("LOAD AIML B")
 
-sessionId = 12345
-sessionData= kernel.getSessionData(sessionId)
-
-kernel.setPredicate("name", "Alex", sessionId)
+#sessionId = 12345
+#sessionData= kernel.getSessionData(sessionId)
+#kernel.setPredicate("name", "Alex", sessionId)
 
 
 # Press CTRL-C to break this loop
 while True:
-    print kernel.respond(raw_input("Enter your message >> "))
+  message = raw_input("Your Message: ")
+  if message == "quit" or message == "QUIT" or message == "EXIT" or message == "exit" or message == "close" or message == "CLOSE":
+	exit()
+  #elif message == "save":
+	#kernel.saveBrain("bot_brain.brn")
+  else:
+	bot_response = kernel.respond(message)
+	print bot_response
